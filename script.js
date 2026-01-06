edd022c84d7b57a21eb7e9afa44362c5ecdbc39d
yutai934357 <luffy20158413@gmail.com>
2026-01-06 23:40:01 +0800

    1/6001

--- script.js
+++ script.js
@@ -143,6 +143,16 @@ function blackhole(element) {
             this.prevY = this.y;
         }
     }
+    const express = require('express');
+const app = express();
+
+// 必須確保有這個根路由
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
     // Event listeners
     const centerHover = document.querySelector('.centerHover');
